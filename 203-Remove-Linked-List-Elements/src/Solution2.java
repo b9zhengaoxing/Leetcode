d062d22a97fae8365b0267e7a92e5434400b7d4c
tengfei li <litengfei@qiyi.com>
2019-10-10 16:15:28 +0800

    203 实现链表删除
    头节点特殊处理
     1，头特殊处理
     2. 遍历，删除所有相同项
     3. 返回header
    虚拟头节点
     1.虚拟头节点，不要特殊处理

--- 203-Remove-Linked-List-Elements/src/Solution2.java
+++ 203-Remove-Linked-List-Elements/src/Solution2.java
@@ -6,18 +6,16 @@
  * ListNode(int x) { val = x; }
  * }
  */
-//思路： 1，头特殊处理 2. 遍历，删除所有相同项 3. 返回header
-class Solution {
+//思路： 1.虚拟头节点，不要特殊处理 2. 遍历，删除所有相同项 3. 返回header
+class Solution2 {
     public ListNode removeElements (ListNode head, int val) {
 
-        while (head != null && head.val == val) {
-            ListNode delNode = head;
-            head = head.next;
-            delNode.next = null;
-        }
+        //虚拟头节点
+        ListNode dumyHead = new ListNode(0);
+        dumyHead.next = head;
 
-        ListNode prev = head;
-        while (prev != null && prev.next != null) { //while 一定要有递增，链表返回head
+        ListNode prev = dumyHead;
+        while (prev.next != null) { //while 一定要有递增，链表返回head
             ListNode delNode = prev.next;
             if (delNode.val == val) {
                 prev.next = delNode.next;
@@ -26,17 +24,6 @@ class Solution {
                 prev = prev.next;
             }
         }
-        return head;
+        return dumyHead.next;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
