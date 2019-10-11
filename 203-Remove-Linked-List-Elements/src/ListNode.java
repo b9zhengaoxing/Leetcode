b8a053e2273e0b9239bc05fd184884bc59b9086b
tengfei li <litengfei@qiyi.com>
2019-10-11 11:05:10 +0800

    203 测试代码书写
    1. 先写测试
    2. 初始化函数不要返回值！！！

--- 203-Remove-Linked-List-Elements/src/ListNode.java
+++ 203-Remove-Linked-List-Elements/src/ListNode.java
@@ -1,5 +1,37 @@
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int x) { val = x; }
+
+    public ListNode (int x) {
+        val = x;
+    }
+
+    //构造函数不能有返回值，Java里面this就是self
+    public ListNode (int[] arr) {
+
+        ListNode prev = this;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                this.val = arr[0];
+            } else {
+                prev.next = new ListNode(arr[i]);
+                prev = prev.next;
+            }
+        }
+    }
+
+    @Override
+    public String toString () {
+
+        StringBuilder res = new StringBuilder();
+        ListNode head = this;
+        while (head != null) {
+            res.append(head.val);
+            if (head.next != null) {
+                res.append("->");
+            }
+            head = head.next;
+        }
+        return res.toString();
+    }
 }
--- 203-Remove-Linked-List-Elements/src/Main.java
+++ 203-Remove-Linked-List-Elements/src/Main.java
@@ -2,6 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-	// write your code here
+        // write your code here
+        int[] arr = {1, 2, 6, 3, 4, 5, 6};
+        ListNode list = new ListNode(arr);
+        System.out.println(list.toString());
+
+        Solution solution = new Solution();
+        solution.removeElements(list,6);
+        System.out.println(list.toString());
     }
 }
